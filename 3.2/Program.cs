f032f7cbbaf479e71fdbb71914d5e8cf9bd1d2e9
S7pt <chaos160702@gmail.com>
2020-05-11 20:13:03 +0300

    Added task 2 files

---
+++ 3.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace _3._2
+{
+    class Program
+    {
+        static void FromFileTextEditor(string path)
+        {
+            string text = File.ReadAllText(path);
+            string[] textToWorkWith = text.Split(" ");
+            int lettersCount = textToWorkWith[0].Length;
+            Console.WriteLine("Your surname has {0} letters", lettersCount);
+            for(int i = 0; i < textToWorkWith[0].Length; i++)
+            {
+                Console.WriteLine(textToWorkWith[0][i]);
+            }
+            int ageToPlus = int.Parse(textToWorkWith[1]);
+            ageToPlus += 10;
+            Console.WriteLine();
+            Console.WriteLine(ageToPlus);
+        }
+        static void Main(string[] args)
+        {
+            string path = @"D:\Task.txt";
+            FromFileTextEditor(path);
+        }
+    }
+}
