52d28c42b0e7a57e5e23eee92f1c8127def7d358
S7pt <chaos160702@gmail.com>
2020-05-11 20:06:42 +0300

    Task 1 added files

---
+++ 3.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+
+namespace _3._1
+{
+    class Program
+    {
+         static string ToLowerRegister(string inputString)
+        {
+            inputString = inputString.ToLower();
+            return inputString;
+        }
+        static void PrintWordsWithEvenNumberOfLetters(string inputString)
+        {
+            string[] editedString = inputString.Split(" ");
+            foreach(var element in editedString)
+            {
+                int counter = element.Length;
+                if (counter % 2 == 0)
+                {
+                    Console.WriteLine(element);
+                }
+            }
+        }
+
+        
+         static void Main(string[] args)
+        {
+            Console.WriteLine("Type any string:");
+           string input = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("1.");
+            Console.WriteLine(ToLowerRegister(input));
+            Console.WriteLine("2.");
+            PrintWordsWithEvenNumberOfLetters(input);
+ }
+}}
+
